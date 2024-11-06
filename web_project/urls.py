9d14b0bef1d6dd3e1216e57b4302ea175e858045
pBui1 <19pbui@chelsea-academy.org>
2024-11-06 13:00:40 +0000

    commit

--- web_project/urls.py
+++ web_project/urls.py
@@ -20,12 +20,13 @@ from django.urls import path
 
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns 
 
 urlpatterns = [
     path("", include("hello.urls")),
     path('admin/', admin.site.urls)
 ]
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns 
+
 urlpatterns += staticfiles_urlpatterns()
 
 
